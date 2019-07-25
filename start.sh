1165991d9e0e3470f311fc6ef6d9c930f5f17e99
morenyangbot <45971234+morenyangbot@users.noreply.github.com>
2019-07-25 21:27:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 mv -f build/libs/todolist-0.0.1-SNAPSHOT.jar /webapp/springboot-todo
 chmod 777 /webapp/springboot-todo/todolist-0.0.1-SNAPSHOT.jar
-nohup java -jar /webapp/springboot-todo/todolist-0.0.1-SNAPSHOT.jar > /webapp/springboot-todo/logfile 2>1 &
+java -jar /webapp/springboot-todo/todolist-0.0.1-SNAPSHOT.jar
